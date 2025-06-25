0c93fd0a8dfcce693f4ea37e20e63c9f5307fbc8
vynguyen73 <nkvyz7303@gmail.com>
2025-06-25 22:46:31 +1000

    Add files via upload
    # SQL Data Exploration – oea_transactions
    
    This file contains basic SQL queries used to explore the `cea_transactions` table in PostgreSQL before visualizing the data in Tableau.
    
    ## Queries Included:
    - View all data
    - Count total transactions
    - Group by client
    - Filter by date

---
+++ oea_transactions_queries.sql
@@ -0,0 +1,16 @@
+
+-- View all data
+SELECT * FROM cea_transactions;
+
+-- Count total transactions
+SELECT COUNT(*) FROM cea_transactions;
+
+-- Group by client
+SELECT client_id, COUNT(*) AS transaction_count
+FROM cea_transactions
+GROUP BY client_id
+ORDER BY transaction_count DESC;
+
+-- Filter by date
+SELECT * FROM cea_transactions
+WHERE order_date >= '2019-09-05';
